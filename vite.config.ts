a2c30ce9be44c7aea429d888c68b2c5b357a253d
sonpablo <pablorodriguez833@gmail.com>
2024-04-20 23:01:46 -0300

    setting up vite-config and package.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,39 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import { peerDependencies, dependencies } from "./package.json";
+import react from "@vitejs/plugin-react";
+import dts from "vite-plugin-dts";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    // react plugin -> parameter js6 runtime. This enable to make bundle a bit smaller
+    react({
+      jsxRuntime: "classic",
+    }),
+    // dts is for building declaration
+    dts({
+      include: ["src/**/*"],
+    }),
+  ],
+  build: {
+    copyPublicDir: false,
+    lib: {
+      entry: resolve(__dirname, "src/index.ts"),
+
+      formats: ["es", "cjs"],
+      fileName: (ext) => `index.${ext}.js`,
+    },
+    // don't include the deps and peerDeps in the build. So, deps and peerDeps are not part of the final library.
+    rollupOptions: {
+      external: [
+        ...Object.keys(peerDependencies),
+        ...Object.keys(dependencies),
+      ],
+      // preserveModules -> important for tree shaking. Combined with sideEffect field from package.json make the lib tree chakeable.
+      output: { preserveModules: true, exports: "named" },
+    },
+
+    target: "esnext",
+    sourcemap: true,
+  },
+});
