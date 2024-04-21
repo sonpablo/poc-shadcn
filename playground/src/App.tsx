5025618baa323098f8a5be7e4ab7f1d607f4a05b
sonpablo <pablorodriguez833@gmail.com>
2024-04-20 23:20:42 -0300

    use lib in playground

--- playground/src/App.tsx
+++ playground/src/App.tsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import "./App.css";
+import "@son-pablo/lib/dist/style.css";
+import { Button } from "@son-pablo/lib";
 
 const MODES = {
   LIGHT: "light",
@@ -7,7 +9,11 @@ const MODES = {
 };
 
 function App() {
-  const [mode] = useState(MODES.LIGHT);
+  const [mode, setMode] = useState(MODES.LIGHT);
+
+  const toggleDarkMode = () => {
+    setMode(mode === MODES.LIGHT ? MODES.DARK : MODES.LIGHT);
+  };
 
   return (
     <main className="main-container">
@@ -15,6 +21,19 @@ function App() {
         <h1 className="text-3xl font-bold underline">
           Playground React + Vite + POC Shadcn wrapped lib
         </h1>
+        <br />
+        <div className="py-4 flex  justify-center">
+          <Button onClick={toggleDarkMode}>Toggle Dark Mode</Button>
+        </div>
+        <div className="py-4 flex  justify-center">
+          <Button
+            variant="destructive"
+            className="text-orange-500"
+            onClick={() => console.log("clic")}
+          >
+            Destructive button
+          </Button>
+        </div>
       </div>
     </main>
   );
